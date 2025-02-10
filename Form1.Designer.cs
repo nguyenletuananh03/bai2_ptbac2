524ec820f1f1c74c2a05b173e0235dd9d6bfc17e
MAY 06 <MAY 06@LAB2-35>
2025-02-10 11:23:01 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace bai2_ptbac2
+{
+    partial class btntinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtso1 = new System.Windows.Forms.TextBox();
+            this.txtso2 = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.bnttinh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nhap a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "nhap b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(197, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "KQ";
+            // 
+            // txtso1
+            // 
+            this.txtso1.Location = new System.Drawing.Point(273, 61);
+            this.txtso1.Name = "txtso1";
+            this.txtso1.Size = new System.Drawing.Size(100, 20);
+            this.txtso1.TabIndex = 1;
+            // 
+            // txtso2
+            // 
+            this.txtso2.Location = new System.Drawing.Point(273, 97);
+            this.txtso2.Name = "txtso2";
+            this.txtso2.Size = new System.Drawing.Size(100, 20);
+            this.txtso2.TabIndex = 1;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(273, 129);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(100, 20);
+            this.txtKQ.TabIndex = 1;
+            // 
+            // bnttinh
+            // 
+            this.bnttinh.Location = new System.Drawing.Point(286, 164);
+            this.bnttinh.Name = "bnttinh";
+            this.bnttinh.Size = new System.Drawing.Size(75, 23);
+            this.bnttinh.TabIndex = 2;
+            this.bnttinh.Text = "tính";
+            this.bnttinh.UseVisualStyleBackColor = true;
+            this.bnttinh.Click += new System.EventHandler(this.bnttinh_Click);
+            // 
+            // btntinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bnttinh);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtso2);
+            this.Controls.Add(this.txtso1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "btntinh";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtso1;
+        private System.Windows.Forms.TextBox txtso2;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button bnttinh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai2_ptbac2
+{
+    public partial class btntinh : Form
+    {
+        public btntinh()
+        {
+            InitializeComponent();
+        }
+
+        private void bnttinh_Click(object sender, EventArgs e)
+        {
+            int intA, intB;
+            double dbekq;
+            Console.WriteLine("------------- CHƯƠNG TRÌNH GIẢI PHƯƠNG TRÌNH BẬC 1------------");
+            Console.WriteLine("---------------------------ax + b = 0 ------------------------");
+            Console.Write("nhập a:");
+            intA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("nhập b:");
+            intB = Convert.ToInt32(Console.ReadLine());
+            if (intA == 0 && intB == 0)
+            {
+                Console.WriteLine(" Phương trình có vô số nghiệm");
+
+            }
+            else if (intA == 0 && intB != 0)
+            {
+                Console.WriteLine("phương trình vô số nghiệm");
+            }
+            else
+            {
+                dbekq = (double)-intB / intA;
+                Console.WriteLine("phương trình có 1 nghiệm duy nhất x = {0}", dbekq);
+            }
+        }
+
+    }
+}
+
